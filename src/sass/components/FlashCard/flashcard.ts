92c8ffdcb003f437e2f4c460bc7e1bb6d7e997ec
VonRiddarn <vonriddarn@gmail.com>
2025-01-15 11:40:04 +0100

    feat: created first draft for card generator

---
+++ src/sass/components/FlashCard/flashcard.ts
@@ -0,0 +1,26 @@
+import "./flashcard.scss";
+
+export type FlashCard = {
+	title: string;
+	imgUrl: string;
+	description : {
+		short: string;
+		long: string;
+	};
+}
+
+export const generateFlashCard = (flashCard: FlashCard) => {
+
+	const article = document.createElement("article");
+	const container = article.appendChild(document.createElement("div"));
+	container.appendChild(document.createElement("h2")).innerHTML = flashCard.title;
+	container.appendChild(document.createElement("h3")).innerHTML = flashCard.description.short;
+	
+	const img = container.appendChild(document.createElement("img"));
+	img.src = flashCard.imgUrl;
+	img.alt = "Image unavailible!";
+
+	container.appendChild(document.createElement("p")).innerHTML = flashCard.description.long;
+
+	return article;
+}
