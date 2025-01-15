ce12af2fb562ccc08efc7bba8c2336572506eff9
VonRiddarn <vonriddarn@gmail.com>
2025-01-15 12:16:30 +0100

    feat: removed the use of div - not needed

--- src/sass/components/FlashCard/flashcard.ts
+++ src/sass/components/FlashCard/flashcard.ts
@@ -16,15 +16,14 @@ export const generateFlashCard = (flashCard: FlashCard) => {
 
 	const article = document.createElement("article");
 	article.classList.add("vontils-cards-flashcard", unflippedSass, unselectableSass);
-	const container = article.appendChild(document.createElement("div"));
-	container.appendChild(document.createElement("h2")).innerHTML = flashCard.title;
-	container.appendChild(document.createElement("h3")).innerHTML = flashCard.description.short;
+	article.appendChild(document.createElement("h2")).innerHTML = flashCard.title;
+	article.appendChild(document.createElement("h3")).innerHTML = flashCard.description.short;
 	
-	const img = container.appendChild(document.createElement("img"));
+	const img = article.appendChild(document.createElement("img"));
 	img.src = flashCard.imgUrl;
 	img.alt = "Image unavailible!";
 
-	container.appendChild(document.createElement("p")).innerHTML = flashCard.description.long;
+	article.appendChild(document.createElement("p")).innerHTML = flashCard.description.long;
 
 	article.addEventListener('click', () => {
 		if(article.classList.contains(unflippedSass))
